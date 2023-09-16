1c1f034f888cfc97018065950c98ad921543c4d3
Bmanz250 <brockmanz55555@gmail.com>
2023-09-16 10:03:57 -0400

    built test app, get location works too

--- smartalert_ui/lib/Button_Page.dart
+++ smartalert_ui/lib/Button_Page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 const startAlignment = Alignment.bottomLeft;
 const endAlignment = Alignment.topRight;
@@ -15,9 +16,46 @@ class ButtonPage extends StatefulWidget {
 
 class _ButtonPageState extends State<ButtonPage> {
   bool buttonPressed = false;
+  LocationData? _locationData; // Store the location data here
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation(); // Call the function when the widget is initialized
+  }
+
   void updateui() {
     setState(() {
       buttonPressed = !buttonPressed;
+    });
+  }
+
+  void _getCurrentLocation() async {
+    Location location = new Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    final locationData = await location.getLocation();
+
+    setState(() {
+      _locationData = locationData; // Update the location data
     });
   }
 
@@ -34,15 +72,25 @@ class _ButtonPageState extends State<ButtonPage> {
       child: Center(
         child: buttonPressed
             ? const Text(
-                'Owner generated prompt goes here',
+                //Put trevor's file here
+                'Owner-generated prompt goes here',
                 style: TextStyle(color: Colors.white),
               )
-            : TextButton(
-                onPressed:
-                    updateui, // You should replace this with the actual onPressed function.
-                child: Image.asset(
-                  'assets/button_image.png',
-                ),
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (_locationData != null)
+                    Text(
+                      'Latitude: ${_locationData!.latitude}, Longitude: ${_locationData!.longitude}',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  TextButton(
+                    onPressed: updateui,
+                    child: Image.asset(
+                      'assets/button_image.png',
+                    ),
+                  ),
+                ],
               ),
       ),
     );
--- smartalert_ui/lib/location_api.dart
+++ smartalert_ui/lib/location_api.dart
@@ -3,23 +3,19 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:location/location.dart';
-
 
 class Post {
-  final Map<String, dynamic> json;
+  final Map<String, dynamic>? json;
 
   Post({this.json});
 
   factory Post.fromJson(Map<String, dynamic> json) {
-    return Post( json );
+    return Post(json: json);
   }
 }
 
 class LocationApi {
-  const locationBaseURI = '';
-  
-  LocationData? _currentLocation;
+  final String locationBaseURI = '';
 
   Future<Post> _getProtocol(double latitude, double longitude) async {
     final Map<String, String> queryParams = {
@@ -27,51 +23,15 @@ class LocationApi {
       'longitude': longitude.toString(),
     };
 
-    final Uri uri = Uri.parse(locationBaseURI).replace(queryParameters: queryParams);
+    final Uri uri =
+        Uri.parse(locationBaseURI).replace(queryParameters: queryParams);
 
-    try {
-      final response = await http.get(uri); // pass in lat and lon
+    final response = await http.get(uri); // pass in lat and lon
 
-      if (response.statusCode == 200) {
-        return Post.fromJson(json.decode(response.body));
-      } else {
-        throw Exception('Failed to load post');
-
-      }
-    } catch (error) {
-      print('Error: $error');
+    if (response.statusCode == 200) {
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load post');
     }
-  }
-
-  /* GET LOCATION FROM PHONE */
-  Future<LocationData> _getLocation() async {
-    Location location = Location();
-
-    bool _serviceEnabled;
-    PermissionStatus _permissionGranted;
-
-    // Check if location services are enabled
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
-
-    // Check if location permissions are granted
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return null;
-      }
-    }
-
-    // Get the current location
-    LocationData locationData = await location.getLocation();
-    setState(() {
-      _currentLocation = locationData;
-    });
   }
 }
